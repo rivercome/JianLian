6f40f23d50c258234b5e7d36f02fe5787397e512
xinzai <1843605478@qq.com>
2018-03-25 22:03:06 +0800

    完成debug
    Change-Id: Iba79788a0e352e3e7d12a8e0e39f1217be89a5a5

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
-// const QiniuPlugin = require('qiniu-webpack-plugin')
-// const config = require('./secret')
+const QiniuPlugin = require('qiniu-webpack-plugin')
+const config = require('./secret')
 module.exports = function (webpackConfig, env) {
   if (env !== 'production') {} else {
     webpackConfig.plugins.push(
@@ -10,13 +10,13 @@ module.exports = function (webpackConfig, env) {
           drop_debugger: true,
           drop_console: true
         }
+      }),
+      new QiniuPlugin({
+        ACCESS_KEY: '3I7GdPEmkgP1TZdOUWxD_20-FBbACXSJNImbBQ37',
+        SECRET_KEY: 'mf-VSlpbhUoUUNbkc2I1gRfPdajhaHNIPNFk6kyx',
+        bucket: 'test',
+        path: 'fe/'
       })
-      // new QiniuPlugin({
-      //   ACCESS_KEY: config.ACCESS_KEY,
-      //   SECRET_KEY: config.SECRET_KEY,
-      //   bucket: 'test',
-      //   path: 'fe/'
-      // })
     )
   }
   return webpackConfig
